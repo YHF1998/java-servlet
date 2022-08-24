9da504601e97214fbd6550f9fef2b7de11620d0a
YHF1998 <714326675@qq.com>
2022-08-24 22:03:37 +0800

    first commit

---
+++ servlet002/src/main/java/com/getdream/servlet/GetServlet.java
@@ -0,0 +1,18 @@
+package com.getdream.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8;");
+        ServletContext context = this.getServletContext();
+        String name = (String) context.getAttribute("name");
+        resp.getWriter().println(name);
+    }
+}
